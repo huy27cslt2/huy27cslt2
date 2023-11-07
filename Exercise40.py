8dc21a0f7e00b4fe0d1c027c97435d6d497df4aa
TTtcenterT <ttcenter@192.168.23.242>
2023-11-07 10:45:53 +0700

    Update Exercise40.py

---
+++ Exercise40.py
@@ -0,0 +1,10 @@
+a=float(input('Enter the side of the first triangle:'))
+b=float(input('Enter the side of the second triangle:'))
+c=float(input('Enter the side of the thirt triangle:'))
+if (a+b>c) and (a+c>b) and (b+c>a) and (a>0) and (b>0) and (c>0):
+    if (a==c) and (a==b):
+        print('Equilateral triangle')
+    elif (a==b) or (a==c) or (b==c):
+        print('Scalene triangle')
+    else:
+        print('Isosceles striangle')
