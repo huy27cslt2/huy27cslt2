46424214a8199c29f082673dd615be503b4ab4ae
TTtcenterT <ttcenter@192.168.23.242>
2023-11-07 11:24:20 +0700

    Update Exercise35.py

--- Exercise35.py
+++ Exercise35.py
@@ -1,8 +1,9 @@
-a=int(input('Nhap so nam con nguoi:'))
+a=int(input('Enter human years:'))
 if (a<=2) and (a>=0):
-    b=a*10,5
+    b=a*10.5
+    print(' Dog years:',b)
 elif a>2:
-    b=2*10,5+(a-2)*4
+    b=2*10.5+(a-2)*4
+    print(' Dog years:',b)
 else:
-    print('Khong hop le!')
-print('So nam con cho:',b)
+    print(' Error!')
