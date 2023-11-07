82c749a82b9e14283ac449bd867e7adc347f001e
oanh43cslt2 <150053173+oanh43cslt2@users.noreply.github.com>
2023-11-07 11:40:29 +0700

    Update Exercise36.py

--- Exercise36.py
+++ Exercise36.py
@@ -1,7 +1,7 @@
-a=input('Nhap chu cai:')
+a=input('Enter a letter of the alphabet:')
 if (a=="u") or (a=='e') or (a=='o') or (a=='a') or (a=='i'):
-    print(a,"la nguyen am")
+    print(a,"is a vowel")
 elif a=='y':
-    print(a,"co the la ngyen am hoac phu am")
+    print(a,"sometimes is a vowel, and sometimes is a consonant")
 else:
-    print(a,"la phu am")
+    print(a,"is a consonant")
