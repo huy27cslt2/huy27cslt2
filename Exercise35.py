0d0106fa3c09559413199f156fcdc2e0901cc668
huy27cslt2 <149955123+huy27cslt2@users.noreply.github.com>
2023-11-08 23:20:13 +0700

    Update Exercise35.py

--- Exercise35.py
+++ Exercise35.py
@@ -1,9 +1,9 @@
 a=int(input('Enter human years:'))
 if (a<=2) and (a>=0):
     b=a*10.5
-    print(' Dog years:',b)
+    print('Dog years:',b)
 elif a>2:
     b=2*10.5+(a-2)*4
-    print(' Dog years:',b)
+    print('Dog years:',b)
 else:
-    print(' Error!')
+    print('Error!')
