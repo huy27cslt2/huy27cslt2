410a169318be52a3fd5bbb8a95f8d5d048ee6f44
huy27cslt2 <149955123+huy27cslt2@users.noreply.github.com>
2023-11-09 21:18:11 +0700

    Update Exercise38.py

--- Exercise38.py
+++ Exercise38.py
@@ -3,7 +3,7 @@ if a in {'January','March','May','July','August','October','December'}:
     print (a,"has 31 days")
 elif a=="Febuary":
     print(a,"has 28 or 29 days")
-elif a in {'Aprill','June','November','September'}:
+elif a in {'April','June','November','September'}:
     print(a,'has 30 days')
 else:
     print('Error!')
