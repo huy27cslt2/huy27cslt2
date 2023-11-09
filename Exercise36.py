fc79d62710bb08a9ca5fc2436411f758d90a4eda
oanh43cslt2 <150053173+oanh43cslt2@users.noreply.github.com>
2023-11-09 16:06:45 +0700

    Update Exercise36.py

--- Exercise36.py
+++ Exercise36.py
@@ -1,6 +1,6 @@
 a=input('Enter a letter of the alphabet:')
 if a.isalpha() and len(a)==1 :
-    if  if a in {'a', 'e', 'i', 'o', 'u'}:
+    if a in {'a', 'e', 'i', 'o', 'u'}:
         print(a,"is a vowel")
     elif a=='y':
         print(a,"sometimes is a vowel, and sometimes is a consonant")    
