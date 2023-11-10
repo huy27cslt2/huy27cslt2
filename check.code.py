877e02ba62431b404dc47472ae8b7d35021f429b
huy27cslt2 <149955123+huy27cslt2@users.noreply.github.com>
2023-11-10 11:21:31 +0700

    Update check.code.py

--- check.code.py
+++ check.code.py
@@ -1,1 +1,7 @@
 print('xin chao giang vien')
+# cách để đẩy file lên github
+# git add . để đẩy full file
+#git  commit -m "first commit"
+# git remote add origin link
+# git push -u origin main
+#tải xuống: git clone link
